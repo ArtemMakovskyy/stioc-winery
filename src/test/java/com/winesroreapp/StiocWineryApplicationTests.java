0066484b99899832f81e96da33a7c9e410f22977
Artem Makovskyy <photomak79@gmail.com>
2024-05-22 14:41:34 +0300

    first commit

---
+++ src/test/java/com/winesroreapp/StiocWineryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winesroreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StiocWineryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
